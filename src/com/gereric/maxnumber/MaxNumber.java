5af66f3adf674df17e956efa989d8129bad444a0
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-21 10:31:19 +0530

    [Vaibhav] added maximum integer among three integer using compareTo()

--- src/com/gereric/maxnumber/MaxNumber.java
+++ src/com/gereric/maxnumber/MaxNumber.java
@@ -2,9 +2,22 @@ package com.gereric.maxnumber;
 
 public class MaxNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void findMaximum(Integer x,Integer y,Integer z)
+	{
+		if(x.compareTo(y)>0 && x.compareTo(z)>0)
+			System.out.println("Maximum number is X = "+x);
+		else if(y.compareTo(x)>0 && y.compareTo(z)>0)
+			System.out.println("Maximum number is Y = "+y);
+		else
+			System.out.println("Maximum number is Z = "+z);
 	}
+	public static void main(String[] args) 
+	{
+		Integer x=70;
+		Integer y=20;
+		Integer z=30;
+		findMaximum(x,y,z);
+	}
+
 
 }
