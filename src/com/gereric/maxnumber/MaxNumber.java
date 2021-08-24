53603bfb8f3b985c4af37ec6609085e6eb7d1ce2
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-24 09:42:10 +0530

    [Vaibhav] added refactored to one generic method

--- src/com/gereric/maxnumber/MaxNumber.java
+++ src/com/gereric/maxnumber/MaxNumber.java
@@ -1,8 +1,8 @@
 package com.gereric.maxnumber;
 
-public class MaxNumber {
-
-	public static void findMaximum(String x,String y,String z)
+public class MaxNumber 
+{
+	public static<T extends Comparable<T>>void findMaximum(T x,T y,T z)
 	{
 		if(x.compareTo(y)>0 && x.compareTo(z)>0)
 			System.out.println("Maximum String is X = "+x);
@@ -13,8 +13,12 @@ public class MaxNumber {
 	}
 	public static void main(String[] args) 
 	{
+		int a=10,b=5,c=20;
+		float l=500f,m=20f,n=90f;
 		String x="Ajay",y="Akshay",z="Rohit";
+		findMaximum(a,b,c);
 		findMaximum(x,y,z);
+		findMaximum(l,m,n);
 	}
 
 
